07e38c75503d421c06649e78e3305138c624c1d1
Andre Wiggins <andrewiggins@live.com>
2024-10-30 17:39:13 -0700

    Add production profiling

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactProductionProfiling: true,
+  experimental: {
+    serverMinification: false,
+  },
 };
 
 export default nextConfig;
