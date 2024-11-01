4fea14cf669bd3cdce5820d9163a639cbbdd1237
Andre Wiggins <andrewiggins@live.com>
2024-10-31 18:34:05 -0700

    Update patch

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,12 @@ const nextConfig: NextConfig = {
   experimental: {
     serverMinification: false,
   },
+  logging: {
+    fetches: {
+      fullUrl: true,
+      hmrRefreshes: true,
+    },
+  },
 };
 
 export default nextConfig;
